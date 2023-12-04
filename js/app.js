cefe8eafafb39dc3b7645dde4a87792a584cc03e
bruno.alarcon <alarconbruno14@gmail.com>
2023-12-03 22:23:41 -0300

    agrego ejercicio 6 terminado

---
+++ js/app.js
@@ -0,0 +1,35 @@
+let temporizadorIniciado = false;
+let tiempoRestante = 0;
+let temporizador = document.querySelector('.temporizador');
+let inputTiempo = document.getElementById('inputTiempo');
+
+function iniciarTiempo() {
+    if (!temporizadorIniciado) {
+        tiempoRestante = inputTiempo.value || 0;
+        temporizadorIniciado = setInterval(actualizarTemporizador, 1000);
+    }
+}
+
+function pausarTemporizador() {
+    clearInterval(temporizadorIniciado);
+    temporizadorIniciado = false;
+}
+
+function resetearTemporizador() {
+    clearInterval(temporizadorIniciado);
+    temporizadorIniciado = false;
+    temporizador.innerText = '00 : 00';
+}
+
+function actualizarTemporizador() {
+    if (tiempoRestante <= 0) {
+        clearInterval(temporizadorIniciado);
+        temporizadorIniciado = null;
+        return;
+    }
+    tiempoRestante--;
+    const minutos = Math.floor(tiempoRestante / 60);
+    const segundos = tiempoRestante % 60;
+    temporizador.textContent = `${minutos.toString().padStart(2, '0')} : ${segundos.toString().padStart(2, '0')}`;
+}
+
