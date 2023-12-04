65845ddd628ddbab79a1c88cf3c6f1aba821a160
bruno.alarcon <alarconbruno14@gmail.com>
2023-12-03 22:29:14 -0300

    realizo correcciones en archivo app.js

--- js/app.js
+++ js/app.js
@@ -1,35 +1,41 @@
 let temporizadorIniciado = false;
 let tiempoRestante = 0;
-let temporizador = document.querySelector('.temporizador');
-let inputTiempo = document.getElementById('inputTiempo');
+let temporizador = document.querySelector(".temporizador");
+let inputTiempo = document.getElementById("inputTiempo");
 
 function iniciarTiempo() {
-    if (!temporizadorIniciado) {
-        tiempoRestante = inputTiempo.value || 0;
-        temporizadorIniciado = setInterval(actualizarTemporizador, 1000);
+  if (!temporizadorIniciado) {
+    if (parseInt(inputTiempo.value) >= 0) {
+      tiempoRestante = parseInt(inputTiempo.value);
+    } else {
+      tiempoRestante = 0;
     }
+
+    temporizadorIniciado = setInterval(actualizarTemporizador, 1000);
+  }
 }
 
 function pausarTemporizador() {
-    clearInterval(temporizadorIniciado);
-    temporizadorIniciado = false;
+  clearInterval(temporizadorIniciado);
+  temporizadorIniciado = false;
 }
 
 function resetearTemporizador() {
-    clearInterval(temporizadorIniciado);
-    temporizadorIniciado = false;
-    temporizador.innerText = '00 : 00';
+  clearInterval(temporizadorIniciado);
+  temporizadorIniciado = false;
+  temporizador.innerText = "00 : 00";
 }
 
 function actualizarTemporizador() {
-    if (tiempoRestante <= 0) {
-        clearInterval(temporizadorIniciado);
-        temporizadorIniciado = null;
-        return;
-    }
-    tiempoRestante--;
-    const minutos = Math.floor(tiempoRestante / 60);
-    const segundos = tiempoRestante % 60;
-    temporizador.textContent = `${minutos.toString().padStart(2, '0')} : ${segundos.toString().padStart(2, '0')}`;
+  if (tiempoRestante <= 0) {
+    clearInterval(temporizadorIniciado);
+    temporizadorIniciado = null;
+    return;
+  }
+  tiempoRestante--;
+  const minutos = Math.floor(tiempoRestante / 60);
+  const segundos = tiempoRestante % 60;
+  temporizador.textContent = `${minutos
+    .toString()
+    .padStart(2, "0")} : ${segundos.toString().padStart(2, "0")}`;
 }
-
